668dfe81cb4f0dd5901d82ed05d917e9da9ce578
Agung Nawawi <nawawiagung354@gmail.com>
2026-07-27 09:09:54 +0700

    fix: route login

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
 
 Route::get(
     '/invoice/{salesTransaction}/print',
